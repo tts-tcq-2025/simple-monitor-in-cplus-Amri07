31862711f04743facd709a2fb8f9696660a55114
Amri07 <128670136+Amri07@users.noreply.github.com>
2025-08-05 12:18:19 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -15,10 +15,17 @@ struct BatteryParameter {
 };
 
 bool checkParameter(const BatteryParameter& param) {
-    if((param.checkMin && param.value < param.min) ||
-       (param.checkMax && param.value > param.max)) {
-        cout << param.outOfRangeMsg << endl;
-        return false;
+    if(param.checkMin) {
+        if(param.value < param.min) {
+            cout << param.outOfRangeMsg << endl;
+            return false;
+        }
+    }
+    if(param.checkMax) {
+        if(param.value > param.max) {
+            cout << param.outOfRangeMsg << endl;
+            return false;
+        }
     }
     return true;
 }
